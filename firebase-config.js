adf34f2f2e0249ab91bbda98bea86409fb42382f
muhammadhaikal45 <mhdmhaikal123@gmail.com>
2026-09-12 23:41:25 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, onValue, set, push, onDisconnect, serverTimestamp, remove } 
+    from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBUi4Mln618DzeMWC6AU0VRUAVE65TXWiY",
-  authDomain: "cesarxrat-1f53c.firebaseapp.com",
-  databaseURL: "https://cesarxrat-1f53c-default-rtdb.firebaseio.com",
-  projectId: "cesarxrat-1f53c",
-  storageBucket: "cesarxrat-1f53c.firebasestorage.app",
-  messagingSenderId: "199773283777",
-  appId: "1:199773283777:web:9169150be954756898fb40"
+    apiKey: "AIzaSyBUi4Mln618DzeMWC6AU0VRUAVE65TXWiY",
+    authDomain: "cesarxrat-1f53c.firebaseapp.com",
+    databaseURL: "https://cesarxrat-1f53c-default-rtdb.firebaseio.com",
+    projectId: "cesarxrat-1f53c",
+    storageBucket: "cesarxrat-1f53c.firebasestorage.app",
+    messagingSenderId: "199773283777",
+    appId: "1:199773283777:web:9169150be954756898fb40"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, onValue, set, push, onDisconnect, serverTimestamp, remove };
